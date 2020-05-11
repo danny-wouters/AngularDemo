c774aaa92bb84ceb223d20ea7b292cf1ca7a8ba6
Danny Wouters <danny_wouters9@hotmail.com>
2020-05-11 22:20:44 +0200

    Move TS, HTML, CSS code from app component to demo component

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -8,36 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular Demo';
   public name = 'World';
-  public greeting = '';
-  public myId = "testId";
-  public isDisabled = true;
-  public successClass = "text-success";
-  public hasError = true;
-  public isSpecial = true;
-  public messageClasses = {
-    'text-success': !this.hasError,
-    'text-danger': this.hasError,
-    'text-special': this.isSpecial
-  };
-  public highlightColor = 'orange';
-  public titleStyles = {
-    color: 'blue',
-    fontStyle: 'italic'
-  };
-  public displayName = false;
-  public color = 'red';
-  public colors = ['red', 'blue', 'green', 'yellow'];
+
 
   greetUser() {
     return 'Hello ' + this.name;
-  }
-
-  onClick(event) {
-    console.log(event.type);
-    this.greeting = 'Welcome to Demo app';
-  }
-
-  logMessage(value) {
-    console.log(value);
   }
 }
--- HelloWorld/src/app/demo/demo.component.ts
+++ HelloWorld/src/app/demo/demo.component.ts
@@ -6,7 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo.component.css']
 })
 export class DemoComponent implements OnInit {
+  public name = 'World';
+  public greeting = '';
+  public myId = 'testId';
+  public isDisabled = true;
+  public successClass = 'text-success';
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+    'text-success': !this.hasError,
+    'text-danger': this.hasError,
+    'text-special': this.isSpecial
+  };
+  public highlightColor = 'orange';
+  public titleStyles = {
+    color: 'blue',
+    fontStyle: 'italic'
+  };
+  public displayName = false;
+  public color = 'red';
+  public colors = ['red', 'blue', 'green', 'yellow'];
 
+  onClick(event) {
+    console.log(event.type);
+    this.greeting = 'Welcome to Demo app';
+  }
+
+  logMessage(value) {
+    console.log(value);
+  }
   constructor() { }
 
   ngOnInit() {
