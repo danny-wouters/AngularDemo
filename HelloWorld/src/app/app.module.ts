d4e612241c0a787f90e31023f911d6338b82c5c4
Danny Wouters <danny_wouters9@hotmail.com>
2020-05-16 16:39:06 +0200

    Add Service example + add employee-list and employee-detail components

--- HelloWorld/src/app/app.module.ts
+++ HelloWorld/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { EmployeeService } from './employee.service';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      DemoComponent
+      DemoComponent,
+      EmployeeDetailComponent,
+      EmployeeListComponent
    ],
    imports: [
       BrowserModule,
       FormsModule
    ],
-   providers: [],
+   providers: [
+      EmployeeService
+   ],
    bootstrap: [
       AppComponent
    ]
