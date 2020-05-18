44be203227d3c7f6aebb4c12a7b8c50f7e410d02
Danny Wouters <danny_wouters9@hotmail.com>
2020-05-18 21:22:04 +0200

    Add ParamMap Observable example

--- RoutingDemo/src/app/department-detail/department-detail.component.ts
+++ RoutingDemo/src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -10,10 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 export class DepartmentDetailComponent implements OnInit {
   public departmentId;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.departmentId = parseInt(this.route.snapshot.paramMap.get('id'));
+    // this.departmentId = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.departmentId = parseInt(params.get('id'));
+    });
+  }
+
+  goPrevious() {
+    let previousId = this.departmentId - 1;
+    this.router.navigate(['departments', previousId]);
+  }
+
+  goNext() {
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['departments', nextId]);
   }
 
 }
